3753e9623699401a1a118f91d0f9aa9a4d722a85
Kushagra Sharma <sparrow.kushagra@gmail.com>
2023-11-16 18:15:09 +0530

    deploying

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { HiAcademicCap } from "react-icons/hi";
+
+
 
 const Home = () => {
   return (
     <div>
+      <HiAcademicCap className='h-10 w-10' />
       <p>Hey,</p>
       <p>This is Kushagra</p>
     </div>
