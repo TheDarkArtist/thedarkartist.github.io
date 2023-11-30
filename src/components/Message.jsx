c18a9c6a561be13a61b4d5217748774ef21a2a89
Kushagra Sharma <sparrow.kushagra@gmail.com>
2023-11-30 18:25:40 +0530

    ++ changed border of message, now looking much nicer

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import { FcSupport } from "react-icons/fc";
 
 const Message = () => {
   return (
-    <div className="px-2 py-1  border border-pink-500 bg-gradient-to-br from-red-950 via-cyan-950 to-red-950  flex items-center z-40 shadow-md shadow-zinc-700 hover:font-bold" >
+    <div className="px-2 py-1  border-t border-b border-pink-500 bg-gradient-to-br from-red-950 via-cyan-950 to-red-950  flex items-center z-40 shadow-md shadow-zinc-700 hover:font-bold" >
       <FcSupport className=" mr-2" /> 
       <p>Under Development</p>
     </div>
