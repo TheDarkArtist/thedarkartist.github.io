5a83bfa87fcfeafe3463d1b05165c8265e0b6501
Kushagra Sharma <sparrow.kushagra@gmail.com>
2023-12-25 21:25:07 +0530

    added Firebase.js

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//
+const firebaseConfig = {
+  apiKey: "AIzaSyAjGPFLnczMaTa_3K5gbLnjLaGmyIb4eBY",
+  authDomain: "thedarkartist-b88c0.firebaseapp.com",
+  projectId: "thedarkartist-b88c0",
+  storageBucket: "thedarkartist-b88c0.appspot.com",
+  messagingSenderId: "469086560056",
+  appId: "1:469086560056:web:9a0c620dd494ff32cc6bcd",
+  measurementId: "G-44SJSYDF44"
+};
+
+const app = initializeApp(firebaseConfig);
